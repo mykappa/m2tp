eec9d5e85a588903c8ffcfad2873854365ca9abe
mykappa <60090249+mykappa@users.noreply.github.com>
2023-04-06 18:05:18 +0200

    Reformat main code

--- m2tp.m
+++ m2tp.m
@@ -1,11 +1,11 @@
-function [status,cmd_out_str] = m2tp(tikz_fn,fig_size,varargin)
-%M2TPREV matlab2tikz preview
-%   Generates a PDF preview of a tikz image created by matlab2tikz using 
+function [status, cmd_out_str] = m2tp(tikz_fn, fig_size, varargin)
+%M2TP matlab2tikz preview
+%   Generates a PDF preview of a tikz image created by matlab2tikz using
 %   standalone compilation (requires working LaTeX environment).
 %
 %   (c) 2021 - Markus Kohler
-% 
-% 
+%
+%
 %   INPUTS:
 %       tikz_fn:            path to tikz filen to be compiled
 %       fig_size:           PDF file size [width, height]
@@ -41,14 +41,14 @@ default_width_def_latex = '\figurewidth';
 
 % Define required inputs and key value pair parameters
 p.addRequired('tikz_fn', @(x) isStringScalar(x) || ischar(x))
-p.addRequired('fig_size', @(x) isnumeric(x) && numel(x)==2)
+p.addRequired('fig_size', @(x) isnumeric(x) && (numel(x) == 2))
 p.addParameter('fig_size_unit', default_length_unit,...
-    @(x) any(validatestring(x,latex_length_units)))
+    @(x) any(validatestring(x, latex_length_units)))
 p.addParameter('pdf_fn', default_pdf_fn,...
     @(x) isStringScalar(x) || ischar(x));
 p.addParameter('pdf_fn_suff', default_pdf_fn_suff,...
     @(x) isStringScalar(x) || ischar(x));
-p.addParameter('tex_wrapper_fn',default_tex_wrapper_fn,...
+p.addParameter('tex_wrapper_fn', default_tex_wrapper_fn,...
     @(x) isStringScalar(x) || ischar(x));
 p.addParameter('cmd_out', default_cmd_out,...
     @(x) islogical(x) && isscalar(x));
@@ -60,22 +60,26 @@ p.addParameter('height_def_latex', default_height_def_latex,...
     @(x) isStringScalar(x) || ischar(x));
 
 % Parse inputs
-parse(p,tikz_fn,fig_size,varargin{:})
+parse(p, tikz_fn, fig_size, varargin{:})
 
 % Compose LaTeX TikZ command strings
 length_def = sprintf('\\newlength%s \\newlength%s',...
-    p.Results.width_def_latex,p.Results.height_def_latex);
-length_set = sprintf('\\setlength\\figurewidth{%.2f%s} \\setlength\\figureheight{%.2f%s}',...
-    p.Results.fig_size(1),p.Results.fig_size_unit,p.Results.fig_size(2),p.Results.fig_size_unit);
-fn_set = sprintf('\\def\\filename{%s}',tikz_fn);
+    p.Results.width_def_latex, p.Results.height_def_latex);
+length_set = sprintf(...
+    '\\setlength\\figurewidth{%.2f%s} \\setlength\\figureheight{%.2f%s}',...
+    p.Results.fig_size(1), p.Results.fig_size_unit,...
+    p.Results.fig_size(2), p.Results.fig_size_unit);
+fn_set = sprintf('\\def\\filename{%s}', tikz_fn);
 
-% Define LaTeX jobname 
-job_name = nextname(p.Results.pdf_fn,sprintf('%s1',p.Results.pdf_fn_suff),'.pdf');
+% Define LaTeX jobname
+job_name = nextname(p.Results.pdf_fn, sprintf('%s1', p.Results.pdf_fn_suff), '.pdf');
 [~, job_name] = fileparts(job_name);
 
 % Compile TikZ image with corresponding standalone LaTeX template
-comp_cmd_str = sprintf('pdflatex.exe -synctex=1 -interaction=nonstopmode -jobname=%s """%s %s %s \\input{%s} """',...
-    job_name,length_def,length_set,fn_set,strrep(fullfile(which(p.Results.tex_wrapper_fn)),'\','\\'));
+comp_cmd_str = sprintf(...
+    'pdflatex.exe -synctex=1 -interaction=nonstopmode -jobname=%s """%s %s %s \\input{%s} """',...
+    job_name, length_def, length_set, fn_set,...
+    strrep(fullfile(which(p.Results.tex_wrapper_fn)), '\', '\\'));
 
 % cmd output handling
 if p.Results.cmd_out
@@ -86,14 +90,22 @@ end
 
 if ~status
     % Open compiled PDF
-    open(sprintf('%s.pdf',job_name))
+    open(sprintf('%s.pdf', job_name))
 
     % Clean auxiliary files
     if p.Results.cl_aux_files
-        if exist(sprintf('%s.aux',job_name),'file'), delete(sprintf('%s.aux',job_name)), end
-        if exist(sprintf('%s.log',job_name),'file'), delete(sprintf('%s.log',job_name)), end
-        if exist(sprintf('%s.synctex.gz',job_name),'file'), delete(sprintf('%s.synctex.gz',job_name)), end
-        if exist(sprintf('%s.out',job_name),'file'), delete(sprintf('%s.out',job_name)), end
+        if exist(sprintf('%s.aux', job_name),'file')
+            delete(sprintf('%s.aux', job_name))
+        end
+        if exist(sprintf('%s.log', job_name),'file')
+            delete(sprintf('%s.log', job_name))
+        end
+        if exist(sprintf('%s.synctex.gz', job_name),'file')
+            delete(sprintf('%s.synctex.gz', job_name))
+        end
+        if exist(sprintf('%s.out', job_name),'file')
+            delete(sprintf('%s.out', job_name))
+        end
     end
 
 end
